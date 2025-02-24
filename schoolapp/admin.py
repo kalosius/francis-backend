8a1720ed6b99a2b8badf7ebbeda03f8ccd4ea058
kalosius <kalosius43@gmail.com>
2025-02-24 11:31:28 +0300

    title admin

--- schoolapp/admin.py
+++ schoolapp/admin.py
@@ -3,3 +3,9 @@ from . models import SeniorOne
 
 # Register your models here.
 admin.site.register(SeniorOne)
+
+
+# Customize the admin site
+admin.site.site_header = "ST.Francis School Admin"
+admin.site.site_title = "ST.Francis Admin Portal"
+admin.site.index_title = "Welcome to ST.Francis Administration"
