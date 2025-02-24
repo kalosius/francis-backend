0ceb68d4e5432dfb93bfd17c0fbab8f43a2d9605
kalosius <kalosius43@gmail.com>
2025-02-24 10:01:16 +0300

    allowed hosts

--- school/settings.py
+++ school/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-xa%9k@4w#c$2e5+sb*bnkzauls=*j97jx*=gwb(v_u0c5ziu07
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
