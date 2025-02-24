da2d97cbdc9d6a7f1fca64f43f00821319e069f6
kalosius <kalosius43@gmail.com>
2025-02-24 13:01:26 +0300

    pages created

--- schoolapp/views.py
+++ schoolapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import SeniorOneForm
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -8,6 +9,7 @@ def index(request):
         form = SeniorOneForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+        return render(request, 'success.html')
     else:
         form = SeniorOneForm()
     return render(request, 'index.html', {'form': form})
